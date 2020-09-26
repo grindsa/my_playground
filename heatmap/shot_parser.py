dc1096e66c88357202b746bbca03da819998b7f3
grindsa <grindelsack@gmail.com>
2020-09-26 19:42:15 +0200

    heatmap

--- heatmap/shot_parser.py
+++ heatmap/shot_parser.py
@@ -7,32 +7,37 @@ if __name__ == '__main__':
     INFILE = 'heatmap/shots.txt'
 
     shot_dic = {}
+    mapping_dic = {}
     fso = open(INFILE, "r")
     cnt = 0
+
+    # cluster size
+    clul = 20
+
     for line in fso:
         cnt += 1
         line = line.rstrip()
 
         # splint line in x and y
         (myx, myy) = line.split(';')
+        calc_x = float(myx)
+        calc_y = float(myy)
 
         # x = -100, 100
         # y = 0, 105
         # calculate coordicates
-        calc_x = round((float(myx) + 100) * 295/100)
-        calc_y = round((105 - float(myy)) * 595/105)
+        calc_x = round((calc_x + 100) * 295/100)
+        calc_y = round((105 - calc_y) * 595/105)
 
-        clul = 19
-        # round to next 10
+        # round to clul
         calc_x = round((calc_x/clul)) * clul
         calc_y = round((calc_y/clul)) * clul
-        print(calc_x, calc_y)
 
-        line = '{0}; {1}'.format(calc_x, calc_y)
-        if line in shot_dic:
-            shot_dic[line] += 1
+        mapping = '{0}; {1}'.format(calc_x, calc_y)
+        if mapping in shot_dic:
+            shot_dic[mapping] += 1
         else:
-            shot_dic[line] = 1
+            shot_dic[mapping] = 1
 
     shot_list = []
     maxl = 0
@@ -40,10 +45,10 @@ if __name__ == '__main__':
         (myx, myy) = shot.split(';')
         if shot_dic[shot] > maxl:
             maxl = shot_dic[shot]
-            print(maxl)
 
         shot_list.append({'x': myx, 'y': myy, 'value': shot_dic[shot]})
 
     fso = open('heatmap/shots.json', "w")
-    fso.write('shots =\'' + json.dumps(shot_list) +'\'')
+    fso.write('shots = \'' + json.dumps(shot_list) +'\'\n')
+    fso.write('jmax = ' + str(maxl))
     fso.close
