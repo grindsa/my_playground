8213557b8dc290ee9422c2ff121d2ade724d9554
grindsa <grindelsack@gmail.com>
2022-01-18 13:21:43 +0100

    foo

---
+++ monkey_patcher/monkey_patch.py
@@ -0,0 +1,17 @@
+#/usr/bin/python3
+
+
+def foobar():
+
+
+    def foo():
+        print('foo')
+
+    def bar():
+        print('bar')
+
+    return foo()
+
+if __name__ == '__main__':
+
+    foobar()
